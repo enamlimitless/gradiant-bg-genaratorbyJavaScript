057c13cda925a2946ecfa2a5b3864b830c5a42f0
enamulhaque7 <enamulhaquemb01@gmail.com>
2021-02-04 21:54:42 +0600

    updating

--- custom.js
+++ custom.js
@@ -1,3 +1,4 @@
+const css = document.querySelector("h4");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const body = document.getElementById("bg");
@@ -9,6 +10,8 @@ function setGradient(){
     +(",")
     +color2.value
     +")";
+
+    css.textContent = body.style.background + ";";
 }
 // color1.addEventListener("input", setGradient);
 // color2.addEventListener("input", setGradient);
